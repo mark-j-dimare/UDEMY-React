269fbd07f791439a04714a63fe65c29303911e4b
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2024-10-05 13:54:47 -0400

    update Quiz.jsx
    Deriving Values, Outputting Questions & Registering Answers

--- 09-react-quiz/src/components/Quiz.jsx
+++ 09-react-quiz/src/components/Quiz.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
 
+import QUESTIONS from '../questions.js';
+
 export default function Quiz() {
     // Questions and Answers State Management
-    const [activeQuestion, setActiveQuestion] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
 
+    const activeQuestionIndex = userAnswers.length;
+
+    // Function to handle the selected answer
+    function handleSelectAnswer(selectedAnswer) {
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer];
+        });
+    }
+
     return (
-        <p>Currently Active Question</p>
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
