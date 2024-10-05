c5d470e78b3c27f298c720c202a2abe4b7aa2024
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2024-10-05 14:30:49 -0400

    Shuffling Answers & Adding Quiz Logic

--- 09-react-quiz/src/components/Quiz.jsx
+++ 09-react-quiz/src/components/Quiz.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 
+import quizCompleteImg from "../assets/quiz-complete.png";
+
 import QUESTIONS from '../questions.js';
 
 export default function Quiz() {
     // Questions and Answers State Management
     const [userAnswers, setUserAnswers] = useState([]);
-
     const activeQuestionIndex = userAnswers.length;
+
+    // Quiz is complete
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     // Function to handle the selected answer
     function handleSelectAnswer(selectedAnswer) {
@@ -15,15 +19,30 @@ export default function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Quiz is complete" />
+                <h2>Quiz Complete!</h2>
+            </div>
+        );
+    }
+
+    // Create a new answers array to not edit the original, since the first answer is correct in the data
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                            <button onClick={() => handleSelectAnswer(answer)}>
+                                {answer}
+                            </button>
                         </li>
                     ))}
                 </ul>
