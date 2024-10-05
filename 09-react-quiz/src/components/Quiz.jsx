06e3f09c0d55760a948b72150b56a60073e54543
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2024-10-05 15:55:33 -0400

    updated with useCallback
    still not working

--- 09-react-quiz/src/components/Quiz.jsx
+++ 09-react-quiz/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
@@ -13,11 +13,15 @@ export default function Quiz() {
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     // Function to handle the selected answer
-    function handleSelectAnswer(selectedAnswer) {
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer];
         });
-    }
+    }, []);
+
+    const handleSkipAnswer = useCallback(() => {
+        handleSelectAnswer(null), [handleSelectAnswer]
+    });
 
     if (quizIsComplete) {
         return (
@@ -35,7 +39,7 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
-                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />
+                <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 
                 <ul id="answers">
