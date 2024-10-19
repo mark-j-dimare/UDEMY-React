6bd73972e002a98423ae12bc93ee8b477968a1e1
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2024-10-19 15:49:18 -0400

    adding error state, adding error message

--- 11-http-requests/src/components/AvailablePlaces.jsx
+++ 11-http-requests/src/components/AvailablePlaces.jsx
@@ -1,21 +1,43 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  // loading state
   const [isFetching, setIsFetching] = useState(false);
+  // data
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  // potential error state
+  const [error, setError] = useState();
 
   useEffect(() => {
     setIsFetching(true);
     async function fetchPlaces() {
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch('http://locanlhost:3000/places');
+        const resData = await response.json();
+
+        if (!response.ok) { // 400, 500 error
+          const throwError = new Error('Failed to fetch places data.');
+          throw throwError;
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError(error);
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return (
+      <ErrorPage title="An Error Occurred" message={error.message} />
+    )
+  }
 
   return (
     <Places
