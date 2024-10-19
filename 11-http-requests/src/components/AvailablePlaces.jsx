4536c3ebb4be74ae4565eccb48ad415384100f78
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2024-10-19 15:26:12 -0400

    add loading state to http call

--- 11-http-requests/src/components/AvailablePlaces.jsx
+++ 11-http-requests/src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
+    setIsFetching(true);
     async function fetchPlaces() {
       const response = await fetch('http://localhost:3000/places');
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -18,6 +21,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
