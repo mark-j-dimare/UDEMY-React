6471af06bdca54b025f5749977acda045684ed7d
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2024-10-19 20:18:15 -0400

    sending the data to the backend

--- 11-http-requests/src/http.js
+++ 11-http-requests/src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
     }
     return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({ places: places }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const resData = await response.json();
+
+    if (!response.ok) { // 400, 500 error
+        const throwError = new Error('Failed to update user data.');
+        throw throwError;
+    }
+    return resData.message;
+}
