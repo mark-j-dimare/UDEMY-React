9c5205e82376d9937cca30b247f7c15b8d0b936f
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2024-10-20 19:31:44 -0400

    building reusable Input component

--- 12-forms/src/App.jsx
+++ 12-forms/src/App.jsx
@@ -1,14 +1,14 @@
 import Header from './components/Header.jsx';
-import Signup from './components/Signup.jsx';
-// import Login from './components/Login.jsx';
+// import Signup from './components/Signup.jsx';
+import Login from './components/StateLogin.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Signup />
-        {/* <Login /> */}
+        {/* <Signup /> */}
+        <Login />
       </main>
     </>
   );
