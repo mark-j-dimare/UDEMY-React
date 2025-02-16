de7de95da4899d720ddb902adbaef0bf6c49f0c5
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2025-02-15 22:52:40 -0500

    Checkout Form and Inputs

---
+++ 13-food-ordering-app/src/components/Checkout.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react"
+import Modal from "./UI/Modal.jsx"
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
+import CartContext from "../store/CartContext.jsx"
+import UserProgressContext from "../store/UserProgressContext.jsx";
+import { currencyFormatter } from "../util/formatting";
+
+export default function Checkout() {
+    const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
+
+    const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
+        return totalPrice + item.quantity * item.price;
+    }, 0);
+
+    function handleClose() {
+        userProgressCtx.hideCheckout();
+    }
+
+    return (
+        <Modal onClose={handleClose} open={userProgressCtx.progress === 'checkout'}>
+            <form>
+                <h2>Checkout</h2>
+                <p>Total Ammount: {currencyFormatter.format(cartTotal)}</p>
+                <Input label="Full Name" id="full-name" type="text" required />
+                <Input label="Email Address" id="email" type="email" required />
+                <Input label="Street Address" id="street" type="text" required />
+                <div className="control-row">
+                    <Input label="City" id="city" type="text" required />
+                    <Input label="Postal Code" id="postal-code" type="text" required />
+                </div>
+                <p className="modal-actions">
+                    <Button type="button" onClick={handleClose} textOnly>Close</Button>
+                    <Button>Submit Order</Button>
+                </p>
+            </form>
+        </Modal>
+    )
+}
--- 13-food-ordering-app/src/components/UI/Modal.jsx
+++ 13-food-ordering-app/src/components/UI/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-export default function Modal({ children, open, className = '' }) {
+export default function Modal({ children, open, onClose, className = '' }) {
     const dialog = useRef();
     useEffect(() => {
         const modal = dialog.current;
@@ -13,7 +13,9 @@ export default function Modal({ children, open, className = '' }) {
     }, [open]);
 
     return createPortal(
-        <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+        <dialog ref={dialog} onClose={onClose} className={`modal ${className}`}>
+            {children}
+        </dialog>,
         document.getElementById('modal')
     );
 }
