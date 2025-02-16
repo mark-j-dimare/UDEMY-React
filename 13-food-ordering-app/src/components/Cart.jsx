75b65c0dd728b6339772c766c1d39dd429695f24
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2025-02-15 21:40:05 -0500

    CartItem.jsx and increase/decrease quantity

--- 13-food-ordering-app/src/components/Cart.jsx
+++ 13-food-ordering-app/src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import Modal from "./UI/Modal.jsx"
 import Button from "./UI/Button.jsx";
+import CartItem from "./CartItem.jsx";
 import CartContext from "../store/CartContext.jsx";
 import { currencyFormatter } from "../util/formatting";
 import UserProgressContext from "../store/UserProgressContext.jsx";
@@ -22,9 +23,14 @@ export default function Cart() {
             <h2>Your Cart</h2>
             <ul>
                 {cartCtx.items.map(item => (
-                    <li key={item.id}>
-                        {item.name} - {item.quantity}
-                    </li>
+                    <CartItem
+                        key={item.id}
+                        name={item.name}
+                        quantity={item.quantity}
+                        price={item.price}
+                        onIncrease={() => cartCtx.addItem(item)}
+                        onDecrease={() => cartCtx.removeItem(item.id)}
+                    />
                 ))}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
