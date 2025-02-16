7b89ce01066f040b2f886c879be22802a4857aa1
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2025-02-15 21:22:25 -0500

    close modal button

--- 13-food-ordering-app/src/components/Cart.jsx
+++ 13-food-ordering-app/src/components/Cart.jsx
@@ -13,6 +13,10 @@ export default function Cart() {
         return totalPrice + item.quantity * item.price;
     }, 0);
 
+    function handleCloseCart() {
+        userProgressCtx.hideCart();
+    }
+
     return (
         <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
@@ -25,7 +29,7 @@ export default function Cart() {
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
             <p className="modal-actions">
-                <Button textOnly>Close</Button>
+                <Button textOnly onClick={handleCloseCart}>Close</Button>
                 <Button>Go to Checkout</Button>
             </p>
         </Modal>
