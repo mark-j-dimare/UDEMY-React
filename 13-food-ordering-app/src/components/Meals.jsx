8e8aadd4a76141f93d57e4c2705b2789cdeeb93f
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2025-02-16 22:06:33 -0500

    creating useHttp.js file

--- 13-food-ordering-app/src/components/Meals.jsx
+++ 13-food-ordering-app/src/components/Meals.jsx
@@ -1,34 +1,24 @@
-import { useState, useEffect } from "react";
+import useHttp from "../hooks/useHttp.js";
 import MealItem from "./MealItem.jsx";
 
+const requestConfig = {};
+
 export default function Meals() {
-    // manage meals state
-    // starts empty, setLoadedMeals becomes meals after fetchMeals happens
-    const [loadedMeals, setLoadedMeals] = useState([]);
 
-    // create function to fetch the meals data
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
+    const {
+        data: loadedMeals,
+        isLoading,
+        error
+    } = useHttp('http://locahost:3000/meals', requestConfig, []); // url, config, initialData
 
-            if (!response.ok) {
-                // ...
-            }
-
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-
-        fetchMeals();
-    }, []);
-
+    console.log(loadedMeals);
 
     return (
         <ul id="meals">
 
             {loadedMeals.map(meal => (
                 <MealItem key={meal.id} meal={meal} />
-            ))};
+            ))}
         </ul>
     );
 }
