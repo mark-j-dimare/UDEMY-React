ebc7487d09df9d44f15081e3f491d54879377a09
mark-j-dimare <127553452+mark-j-dimare@users.noreply.github.com>
2025-02-16 20:59:46 -0500

    checkout submissions

--- 13-food-ordering-app/src/components/Checkout.jsx
+++ 13-food-ordering-app/src/components/Checkout.jsx
@@ -18,12 +18,32 @@ export default function Checkout() {
         userProgressCtx.hideCheckout();
     }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries()); // { email: test@example.com }
+
+        fetch('http://localhost:3000/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData
+                }
+            })
+        });
+    }
+
     return (
         <Modal onClose={handleClose} open={userProgressCtx.progress === 'checkout'}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Ammount: {currencyFormatter.format(cartTotal)}</p>
-                <Input label="Full Name" id="full-name" type="text" required />
+                <Input label="Full Name" id="name" type="text" required />
                 <Input label="Email Address" id="email" type="email" required />
                 <Input label="Street Address" id="street" type="text" required />
                 <div className="control-row">
